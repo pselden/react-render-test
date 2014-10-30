4aa657717db80c9c0af5f0b619d5fe42a83c9bf6
Paul Selden <pselden@axs.com>
2014-10-30 17:07:37 -0400

    Remove internationalization to isolate performance problems

--- js/components/Numbers.js
+++ js/components/Numbers.js
@@ -1,12 +1,9 @@
 /** @jsx React.DOM */
 'use strict';
 
-var React = require('react'),
-    ReactIntlMixin = require('react-intl');
+var React = require('react');
 
 var Numbers = React.createClass({
-    mixins: [ReactIntlMixin],
-
     getInitialState: function() {
         return {
             numbers: this.props.numbers
@@ -16,7 +13,7 @@ var Numbers = React.createClass({
     render: function() {
         var self = this;
         var list = this.state.numbers.map(function(number){
-            return <li key={number.value}>{number.value} - {self.formatNumber(number.value, {style: 'currency', currency: 'USD'})}</li>
+            return <li key={number.value}>{number.value} - {self.props.locales[0]}</li>
         });
 
         return <ul>{list}</ul>;
--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 /** @jsx React.DOM */
 'use strict';
 
-var React = require('react'),
+var React = require('react/addons'),
     App = require('./components/App');
 
 if ('production' !== process.env.NODE_ENV) {
     window.React = React; // Enable React devtools
 }
 
-React.renderComponent(<App/>, document.body);
+React.renderComponent(<App/>, document.getElementById('main'));
